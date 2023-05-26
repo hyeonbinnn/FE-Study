4907d63deb5c90a66505bdc02f211cbcb1d556e8
hyeonbin jeong <duafuuu@gmail.com>
2023-05-26 16:50:12 +0900

    클로저 활용

--- JavaScript/013_closure_자주 발생하는 실수.js
+++ JavaScript/013_closure_자주 발생하는 실수.js
@@ -1,11 +1,41 @@
-var arr = [];
+// 실수
+// var arr = [];
 
-for (var i = 0; i < 5; i++) {
+// for (var i = 0; i < 5; i++) {
+//   arr[i] = function () {
+//     return i;
+//   };
+// }
+
+// for (var j = 0; j < arr.length; j++) {
+//   console.log(arr[j]());
+// }
+
+// 바르게 동작하는 코드
+// var arr = [];
+
+// for (var i = 0; i < 5; i++) {
+//   arr[i] = (function (id) {
+//     // ②
+//     return function () {
+//       return id; // ③
+//     };
+//   })(i); // ①
+// }
+
+// for (var j = 0; j < arr.length; j++) {
+//   console.log(arr[j]());
+// }
+
+// let 키워드를 사용해 더 깔끔한 코드
+const arr = [];
+
+for (let i = 0; i < 5; i++) {
   arr[i] = function () {
     return i;
   };
 }
 
-for (var j = 0; j < arr.length; j++) {
-  console.log(arr[j]());
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]());
 }
