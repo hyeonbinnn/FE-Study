043a8c6516d17e08a888099cb5a96e2ede871a2d
hyeonbin jeong <duafuuu@gmail.com>
2023-05-26 13:09:43 +0900

    파일명 수정

---
+++ JavaScript/013_closure_정보의 은닉.js
@@ -0,0 +1,19 @@
+function Counter() {
+  // 카운트를 유지하기 위한 자유 변수
+  let counter = 0;
+
+  // 클로저
+  this.increase = function () {
+    return ++counter;
+  };
+
+  // 클로저
+  this.decrease = function () {
+    return --counter;
+  };
+}
+
+const counter = new Counter();
+
+console.log(counter.increase()); // 1
+console.log(counter.decrease()); // 0
