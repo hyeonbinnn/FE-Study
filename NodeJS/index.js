48390e42e1b77817a9a85c909683d30002d60046
hyeonbin jeong <duafuuu@gmail.com>
2024-02-04 22:31:01 +0900

    add: Node.JS 설치 및 몽고 DB 연결

---
+++ NodeJS/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://hyeonbinnn:abcd1234@cluster0.9bcslpt.mongodb.net/?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
