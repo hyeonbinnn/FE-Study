143c57d5a4171a2281bd6263935afb0e631fec68
hyeonbin jeong <duafuuu@gmail.com>
2024-03-01 20:29:28 +0900

    chore: 주석 추가

--- JavaScript/JS-100/002_배열의 내장함수.js
+++ JavaScript/JS-100/002_배열의 내장함수.js
@@ -7,6 +7,11 @@ console.log(arr);
 // ES6 문법 (Array destructuring)
 [arr[3], arr[2]] = [arr[2], arr[3]];
 
+console.log(arr);
+// 출력: [200, 100, 10000, 300]
+
+// 또는
+
 // index 번호 이용해 위치 변경
 var arrr = arr[2];
 arr[2] = arr[3];
@@ -15,10 +20,11 @@ arr[3] = arrr;
 console.log(arr);
 // 출력: [200, 100, 10000, 300]
 
+// 또는
+
 var arrrr = [200, 100, 300];
 
 // splice 메서드는 배열의 요소를 삭제 또는 교체하거나 새 요소를 추가해 배열의 내용을 변경함
 arrrr.splice(2, 0, 10000);
-
 console.log(arrrr);
 // 출력: [200, 100, 10000, 300]
