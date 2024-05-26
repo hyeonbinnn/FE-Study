7781b1b47260824fcaeceb34ffe633bd84b96c39
hyeonbin jeong <duafuuu@gmail.com>
2024-05-27 02:29:33 +0900

    chore: 불필요한 코드 삭제

--- NextJS/basic/app/layout.js
+++ NextJS/basic/app/layout.js
@@ -1,8 +1,5 @@
-import { Inter } from 'next/font/google';
 import './globals.css';
 import Link from 'next/link';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,8 +8,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html>
+      <body>
         <div className="navBar">
           <Link href="/">홈</Link>
           <Link href="/list">List</Link>
