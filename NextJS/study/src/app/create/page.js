d51e39743c83851f4ed3401dbbd394fc00df4f08
hyeonbin jeong <duafuuu@gmail.com>
2024-05-27 22:41:33 +0900

    chore: 기본 세팅 변경

---
+++ NextJS/study/src/app/create/page.js
@@ -0,0 +1,7 @@
+export default function Create() {
+  return <>Create</>;
+}
+
+// export default function Create(props) {
+//   return <>Create : {props.params.id}</>;
+// }
---
+++ NextJS/study/src/app/read/[id]/page.js
@@ -0,0 +1,8 @@
+export default function Read(props) {
+  return (
+    <>
+      <h2>읽기</h2>
+      paramters : {props.params.id}
+    </>
+  );
+}
